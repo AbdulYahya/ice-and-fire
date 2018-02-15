4ea61f7ac34dd78e874b6fe54f597bbdc69e5326
Abdullah Yahya <abdullah@>
2018-02-15 15:48:35 -0800

    Refactored API request methods. Added methods to request all books, books by names, and books by id. As well as characters by name. Displaying sample test data.

--- assets/js/book-interface.js
+++ assets/js/book-interface.js
@@ -1,56 +1,33 @@
 import { IceAndFire } from '../assets/js/ice-and-fire.js';
-
+import * as _ from 'lodash';
 const iceAndFire = new IceAndFire();
 
 $(function() {
-  // console.log(apiURL);
-  // console.warn(iceAndFire.grabBooks(apiURL));
+  // let u = iceAndFire.getBookById(3);
+  // let books = iceAndFire.getAllBooks();
 
-  // let list = iceAndFire.request('characters', 1);
+  let search = document.getElementById('searchBtn');
 
+  search.addEventListener('click', function() {
+    let searchTerm = $('#searchBar').val();
+    let character = iceAndFire.getCharacterByName(searchTerm);
+    console.log(character[0].name);
+    $('#searchBar').val('');
 
-  console.log(iceAndFire.randETag());
-  iceAndFire.grabBook(1).then(book => console.log(book.name));
-  // console.log(iceAndFire.grabBooks(apiURL));
-  // let iterator = list.entries();
-  // let a = c.values();
+    $('#characterInfo').removeClass('hidden');
+    $('#charName').html(`${character[0].name}`);
+  });
 
-  // console.log(iceAndFire.bookArray.length);
-  // for (let book of list) {
-  //   console.log(list);
-  //   console.warn(1);
+  // console.log(books.length);
+  // for (let i = 0; i < books.length; i++) {
+    // console.log(books[i].name);
+  //   $('#books').append('<div class="card">' +
+  //                         '<img class="card-img-top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22214%22%20height%3D%22160%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20214%20160%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1619b88eba5%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A11pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1619b88eba5%22%3E%3Crect%20width%3D%22214%22%20height%3D%22160%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2278.8828125%22%20y%3D%2284.8%22%3E214x160%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="test" />' +
+  //                         '<div class="card-body">' +
+  //                           `<h5 class="card-title"> ${books[i].url.match(/\d+/)}. ${books[i].name}</h5>` +
+  //                           `<p class="card-text">${books[i].authors}</p>` +
+  //                           `<p class="card-text"> Number of Pages <span class="badge badge-primary float-right ">${books[i].numberOfPages}</span></p>` +
+  //                         '</div>' +
+  //                       '</div>');
   // }
-
-  // console.log(a.next().value);
-  // list.forEach(function(item) {
-  //   console.error(item);
-  //   $('#characters').append(`<li>${item}</li>`);
-  // });
-  // let promise = new Promise(function(resolve, reject) {
-  // let request = new XMLHttpRequest();
-  //   let url = `${apiURL}/books/1`;
-  //   // console.log(apiURL);
-  //   request.onload = function() {
-  //     if (this.status === 200) {
-  //       resolve(request.response);
-  //     } else {
-  //       reject(Error(request.statusText));
-  //     }
-  //   };
-  //   request.open('GET', url, true);
-  //   request.send();
-  // });
-  //
-  // promise.then(function(response) {
-  //   let body = JSON.parse(response);
-  //   let characters = body.characters;
-  //   // characters = characters.typeOf;
-  //   // console.log(body.characters);
-  //   console.log(body.characters.length);
-  //   characters.forEach(function(character) {
-  //     $('#characters').append(`<li>${character}</li>`);
-  //   });
-  //
-  //   $('#bookName').text(`${body.name}`);
-  // });
 });
--- assets/js/ice-and-fire.js
+++ assets/js/ice-and-fire.js
@@ -1,60 +1,115 @@
 import * as _ from 'lodash';
-import * as superagent from 'superagent';
+import * as superagent from 'superagent'; // Parses JSON Response for you
 import apiURL from '../../.env';
 
-const cache = {
-  books: {},
-  characters: {},
-  houses: {}
-};
 
 export class IceAndFire {
   constructor() {
-    this.bookArray = [];
-
-
-    console.log(cache);
+    this.cache = {
+      books: {},
+      characters: {},
+      houses: {}
+    };
   }
 
-  request(type, index) {
-    return new Promise((resolve, reject) => {
-      let request = superagent.get(`https://www.anapioficeandfire.com/api/${type}/${index}`);
+  request(url) {
+    let that = this;
+    let apiRequest = new XMLHttpRequest();
+    let promise = new Promise((resolve, reject) => {
 
-      // Accept Header - set a specific api version
-      request.set({Accept: 'application/vnd.anapioficeandfire+json; version=1'});
-
-      // If response objects aren't in our 'cache' add the 'If-None-Match ETag Header'
-      if (cache[type][index]) {
-        request.set({'If-None-Match': cache[type][index].etag});
+      apiRequest.onload = function() {
+        if (this.status === 200) {
+          resolve(apiRequest.response);
+        } else {
+          reject(Error(request.statusText));
+        }
       }
 
-      request.end((error, response) => {
-        // If error
-        if (error && (!response || response.status != 304)) {
-          console.warn(error);
-          return reject(error);
-        }
-
-        if (response != 304) {
-          // Add response to our cache
-          cache[type][index] = {
-            etag: this.randETag(),
-            data: response.body
-          };
-        }
-
-        resolve(cache[type][index].data);
-      });
+      apiRequest.open('GET', url, false);
+      apiRequest.send(null);
     });
+
+    return JSON.parse(apiRequest.responseText);
   }
 
-  grabBook(index) {
-    return this.request('books', index);
+  getAllBooks() {
+    let url = `https://www.anapioficeandfire.com/api/books`;
+    return this.request(url);
   }
+
+  getBookById(bookId) {
+    bookId = bookId.toString();
+    let url = `https://www.anapioficeandfire.com/api/books/${bookId}`;
+    return this.request(url);
+  }
+
+  getBookByName(bookName) {
+    return null;
+  }
+
+  getAllCharacters() {
+    return null;
+  }
+
+  getCharacterById(characterId) {
+    return null;
+  }
+
+  getCharacterByName(characterName) {
+    let url = `https://www.anapioficeandfire.com/api/characters/?name=${characterName}`;
+    return this.request(url);
+  }
+
+  // request()
+
+  // request(type, index) {
+  //   let promise = new Promise((resolve, reject) => {
+  //     let request = superagent.get(`https://www.anapioficeandfire.com/api/${type}/${index}`);
+  //
+  //     // Accept Header - set a specific api version
+  //     request.set({Accept: 'application/vnd.anapioficeandfire+json; version=1'});
+  //
+  //     // If response objects aren't in our 'cache' add the 'If-None-Match ETag Header'
+  //     if (this.cache[type][index]) {
+  //       request.set({'If-None-Match': this.cache[type][index].etag});
+  //     }
+  //
+  //     request.end((error, response) => {
+  //       // If error
+  //       if (error && (!response || response.status != 304)) {
+  //         // console.warn(error);
+  //         return reject(error);
+  //       }
+  //
+  //       if (response != 304) {
+  //         // Add response to our cache
+  //         this.cache[type][index] = {
+  //           etag: this.randETag(),
+  //           data: response.body
+  //         };
+  //       }
+  //
+  //       resolve(this.cache[type][index].data);
+  //     });
+  //   });
+  //
+  //   promise.then(function(response) {
+  //     // console.log(response);
+  //     return response;
+  //   });
+  // }
+  //
+  // grabBook(index) {
+  //   return this.request('books', index);
+  // }
+
+  // grabCharacter(index) {
+  //   return this.request('characters', index);
+  // }
 
   randETag() {
     return Math.random().toString(36).split('').filter(function(value, index, self) {
       return self.indexOf(value) === index;
-    }).join('');
+    }).join('').substring(2, 12);
   }
 }
