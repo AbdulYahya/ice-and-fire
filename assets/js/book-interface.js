bef026d87ce3210cbad746c4c25317aa1913d69b
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-02-15 01:30:32 -0800

    Refactored api request.

--- assets/js/book-interface.js
+++ assets/js/book-interface.js
@@ -3,33 +3,54 @@ import { IceAndFire } from '../assets/js/ice-and-fire.js';
 const iceAndFire = new IceAndFire();
 
 $(function() {
-  console.warn(apiURL);
+  // console.log(apiURL);
+  // console.warn(iceAndFire.grabBooks(apiURL));
 
-  let promise = new Promise(function(resolve, reject) {
-    let request = new XMLHttpRequest();
-    let url = `${apiURL}/books/1`;
-    // console.log(apiURL);
-    request.onload = function() {
-      if (this.status === 200) {
-        resolve(request.response);
-      } else {
-        reject(Error(request.statusText));
-      }
-    };
-    request.open('GET', url, true);
-    request.send();
-  });
+  // let list = iceAndFire.request('characters', 1);
 
-  promise.then(function(response) {
-    let body = JSON.parse(response);
-    let characters = body.characters;
-    // characters = characters.typeOf;
-    // console.log(body.characters);
-    console.log(body.characters.length);
-    characters.forEach(function(character) {
-      $('#characters').append(`<li>${character}</li>`);
-    });
 
-    $('#bookName').text(`${body.name}`);
-  });
+  console.log(iceAndFire.randETag());
+  iceAndFire.grabBook(1).then(book => console.log(book.name));
+  // console.log(iceAndFire.grabBooks(apiURL));
+  // let iterator = list.entries();
+  // let a = c.values();
+
+  // console.log(iceAndFire.bookArray.length);
+  // for (let book of list) {
+  //   console.log(list);
+  //   console.warn(1);
+  // }
+
+  // console.log(a.next().value);
+  // list.forEach(function(item) {
+  //   console.error(item);
+  //   $('#characters').append(`<li>${item}</li>`);
+  // });
+  // let promise = new Promise(function(resolve, reject) {
+  // let request = new XMLHttpRequest();
+  //   let url = `${apiURL}/books/1`;
+  //   // console.log(apiURL);
+  //   request.onload = function() {
+  //     if (this.status === 200) {
+  //       resolve(request.response);
+  //     } else {
+  //       reject(Error(request.statusText));
+  //     }
+  //   };
+  //   request.open('GET', url, true);
+  //   request.send();
+  // });
+  //
+  // promise.then(function(response) {
+  //   let body = JSON.parse(response);
+  //   let characters = body.characters;
+  //   // characters = characters.typeOf;
+  //   // console.log(body.characters);
+  //   console.log(body.characters.length);
+  //   characters.forEach(function(character) {
+  //     $('#characters').append(`<li>${character}</li>`);
+  //   });
+  //
+  //   $('#bookName').text(`${body.name}`);
+  // });
 });
--- assets/js/ice-and-fire.js
+++ assets/js/ice-and-fire.js
@@ -1,6 +1,60 @@
+import * as _ from 'lodash';
+import * as superagent from 'superagent';
 import apiURL from '../../.env';
 
-export class IceAndFire {
-  constructor() {}
+const cache = {
+  books: {},
+  characters: {},
+  houses: {}
+};
 
+export class IceAndFire {
+  constructor() {
+    this.bookArray = [];
+
+
+    console.log(cache);
+  }
+
+  request(type, index) {
+    return new Promise((resolve, reject) => {
+      let request = superagent.get(`https://www.anapioficeandfire.com/api/${type}/${index}`);
+
+      // Accept Header - set a specific api version
+      request.set({Accept: 'application/vnd.anapioficeandfire+json; version=1'});
+
+      // If response objects aren't in our 'cache' add the 'If-None-Match ETag Header'
+      if (cache[type][index]) {
+        request.set({'If-None-Match': cache[type][index].etag});
+      }
+
+      request.end((error, response) => {
+        // If error
+        if (error && (!response || response.status != 304)) {
+          console.warn(error);
+          return reject(error);
+        }
+
+        if (response != 304) {
+          // Add response to our cache
+          cache[type][index] = {
+            etag: this.randETag(),
+            data: response.body
+          };
+        }
+
+        resolve(cache[type][index].data);
+      });
+    });
+  }
+
+  grabBook(index) {
+    return this.request('books', index);
+  }
+
+  randETag() {
+    return Math.random().toString(36).split('').filter(function(value, index, self) {
+      return self.indexOf(value) === index;
+    }).join('');
+  }
 }
