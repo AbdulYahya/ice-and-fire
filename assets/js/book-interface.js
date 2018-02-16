21c2f029a8596540c70a9bed2b8796a965d06784
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-02-16 00:55:36 -0800

    Refactored api retrieval methods to use ES6 async promises correctly (used to be sync). Refactored and cleaned up interface/frontend to make use of the new api methods

--- assets/js/book-interface.js
+++ assets/js/book-interface.js
@@ -1,47 +1,72 @@
 import { IceAndFire } from '../assets/js/ice-and-fire.js';
-import * as _ from 'lodash';
-const iceAndFire = new IceAndFire();
-console.log(iceAndFire);
+
+let iceAndFire = new IceAndFire();
+
+
 $(function() {
-  // let u = iceAndFire.getBookById(3);
-  // let books = iceAndFire.getAllBooks();
+  let search = document.getElementById('searchDropdown');
 
-  let search = document.getElementById('searchBtn');
-
-  search.addEventListener('click', function() {
+  search.addEventListener('change', function() {
     let searchTerm = $('#searchBar').val();
-    let character = iceAndFire.getCharacterByName(searchTerm);
-    // if (searchTerm != '') {
-    //   let character = iceAndFire.getCharacterByName(searchTerm);
-    // } else {
-    //   console.log('Empty!');
-    // }
-
-    console.log(character[0].name);
     $('#searchBar').val('');
-    $('#characterInfo').removeClass('hidden');
-    $('#characterInfo').append('<div class="card-body">' +
-                                  `<h5 class="class-title">${character[0].name}</h5>` +
-                                  '<ul class="list-group list-group-flush">' +
-                                    `<li class="list-group-item">${character[0].gender}</li>` +
-                                    `<li class="list-group-item">${character[0].culture}</li>` +
-                                    `<li class="list-group-item">${character[0].born}</li>` +
-                                    `<li class="list-group-item">${character[0].titles}</li>` +
-                                  '</ul>' +
-                                '</div>');
+    // Grab dropdown select menu 'options' value
+    let selectedSearch = search.options[search.selectedIndex].value;
+    // Reset dropdown select menu to first default option
+    search.options[0].selected = true;
 
+    console.log(`Search Term: ${searchTerm}`);
+    console.log(`Searching: ${selectedSearch}`);
+
+    switch(selectedSearch) {
+      case 'Books':
+        iceAndFire.getBookById(1);
+        // Must wait for our api call to retrieve the requested info
+        // otherwise we try to access data that is not there yet...
+        setTimeout(() => {
+          console.log(iceAndFire.cache.books[1].data.name);
+        }, 650);
+        break;
+      case 'Characters':
+        iceAndFire.getCharacterById(583);
+        setTimeout(() => {
+          let character = iceAndFire.cache.characters[583];
+          // console.log(iceAndFire.cache.characters[583].data.name);
+          console.log(character.data.name);
+          $('#characterInfo').removeClass('hidden');
+
+          $('#characterInfo').append('<div class="card-body">' +
+                                        `<h5 class="class-title">${character.data.name}</h5>` +
+                                        '<ul class="list-group list-group-flush">' +
+                                          `<li class="list-group-item">${character.data.gender}</li>` +
+                                          `<li class="list-group-item">${character.data.culture}</li>` +
+                                          `<li class="list-group-item">${character.data.born}</li>` +
+                                          `<li class="list-group-item">${character.data.titles}</li>` +
+                                        '</ul>' +
+                                      '</div>');
+        }, 750);
+        break;
+      case 'Houses':
+        iceAndFire.getHouseById(2);
+        setTimeout(() => {
+          console.log(iceAndFire.cache.houses[2].data.name);
+        }, 650);
+        break;
+      default:
+        console.log('Nothing to see here!');
+    }
+
+
+    // console.log(books.length);
+    // for (let i = 0; i < books.length; i++) {
+      // console.log(books[i].name);
+    //   $('#books').append('<div class="card">' +
+    //                         '<img class="card-img-top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22214%22%20height%3D%22160%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20214%20160%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1619b88eba5%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A11pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1619b88eba5%22%3E%3Crect%20width%3D%22214%22%20height%3D%22160%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2278.8828125%22%20y%3D%2284.8%22%3E214x160%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="test" />' +
+    //                         '<div class="card-body">' +
+    //                           `<h5 class="card-title"> ${books[i].url.match(/\d+/)}. ${books[i].name}</h5>` +
+    //                           `<p class="card-text">${books[i].authors}</p>` +
+    //                           `<p class="card-text"> Number of Pages <span class="badge badge-primary float-right ">${books[i].numberOfPages}</span></p>` +
+    //                         '</div>' +
+    //                       '</div>');
+    // }
   });
-
-  // console.log(books.length);
-  // for (let i = 0; i < books.length; i++) {
-    // console.log(books[i].name);
-  //   $('#books').append('<div class="card">' +
-  //                         '<img class="card-img-top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22214%22%20height%3D%22160%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20214%20160%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1619b88eba5%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A11pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1619b88eba5%22%3E%3Crect%20width%3D%22214%22%20height%3D%22160%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2278.8828125%22%20y%3D%2284.8%22%3E214x160%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="test" />' +
-  //                         '<div class="card-body">' +
-  //                           `<h5 class="card-title"> ${books[i].url.match(/\d+/)}. ${books[i].name}</h5>` +
-  //                           `<p class="card-text">${books[i].authors}</p>` +
-  //                           `<p class="card-text"> Number of Pages <span class="badge badge-primary float-right ">${books[i].numberOfPages}</span></p>` +
-  //                         '</div>' +
-  //                       '</div>');
-  // }
 });
--- assets/js/ice-and-fire.js
+++ assets/js/ice-and-fire.js
@@ -1,43 +1,66 @@
-import * as _ from 'lodash';
-import * as superagent from 'superagent'; // Parses JSON Response for you
-import apiURL from '../../.env';
-
 export class IceAndFire {
   constructor() {
-    this.books = [];
+    this.cache = {
+       books: [],
+       characters: [],
+       houses: []
+    };
   }
 
-  request(url) {
-    let that = this;
-    let apiRequest = new XMLHttpRequest();
+  request(type, index) {
     let promise = new Promise((resolve, reject) => {
+      let apiRequest = new XMLHttpRequest();
+      let url = `https://www.anapioficeandfire.com/api/${type}/${index}`;
 
       apiRequest.onload = function() {
         if (this.status === 200) {
           resolve(apiRequest.response);
         } else {
-          reject(Error(request.statusText));
+          reject(Error(apiRequest.statusText));
         }
-      }
+      };
 
-      apiRequest.open('GET', url, false);
-      apiRequest.send(null);
+      apiRequest.open('GET', url, true);
+      apiRequest.send();
     });
-    this.books.push(apiRequest.responseText);
-    return JSON.parse(apiRequest.responseText);
+
+    promise.then((response) => {
+      let body = JSON.parse(response);
+
+      this.cache[type][index] = {
+        data: body
+      };
+      // console.warn(this.cache[type][index].data.name);
+      // console.warn(this.cache[type][index].data);
+    });
+
+    return this.cache[type][index];
   }
 
   getAllBooks() {
-    let url = `https://www.anapioficeandfire.com/api/books`;
-    return this.request(url);
+    // let url = `https://www.anapioficeandfire.com/api/books`;
+    // return this.request(url);
+    return null;
   }
 
-  getBookById(bookId) {
-    bookId = bookId.toString();
-    let url = `https://www.anapioficeandfire.com/api/books/${bookId}`;
-    return this.request(url);
+  // *********************************
+  //          Search by ID's         *
+  // *********************************
+  getBookById(index) {
+    return this.request('books', index);
   }
 
+  getCharacterById(index) {
+    return this.request('characters', index);
+  }
+
+  getHouseById(index) {
+    return this.request('houses', index);
+  }
+
+  // *********************************
+  //        Search by Names's        *
+  // *********************************
   getBookByName(bookName) {
     return null;
   }
@@ -46,61 +69,11 @@ export class IceAndFire {
     return null;
   }
 
-  getCharacterById(characterId) {
+  getCharacterByName(characterName) {
+    // let url = `https://www.anapioficeandfire.com/api/characters/?name=${characterName}`;
+    // return this.request(url);
     return null;
   }
-
-  getCharacterByName(characterName) {
-    let url = `https://www.anapioficeandfire.com/api/characters/?name=${characterName}`;
-    return this.request(url);
-  }
-
-  // request()
-
-  // request(type, index) {
-  //   let promise = new Promise((resolve, reject) => {
-  //     let request = superagent.get(`https://www.anapioficeandfire.com/api/${type}/${index}`);
-  //
-  //     // Accept Header - set a specific api version
-  //     request.set({Accept: 'application/vnd.anapioficeandfire+json; version=1'});
-  //
-  //     // If response objects aren't in our 'cache' add the 'If-None-Match ETag Header'
-  //     if (this.cache[type][index]) {
-  //       request.set({'If-None-Match': this.cache[type][index].etag});
-  //     }
-  //
-  //     request.end((error, response) => {
-  //       // If error
-  //       if (error && (!response || response.status != 304)) {
-  //         // console.warn(error);
-  //         return reject(error);
-  //       }
-  //
-  //       if (response != 304) {
-  //         // Add response to our cache
-  //         this.cache[type][index] = {
-  //           etag: this.randETag(),
-  //           data: response.body
-  //         };
-  //       }
-  //
-  //       resolve(this.cache[type][index].data);
-  //     });
-  //   });
-  //
-  //   promise.then(function(response) {
-  //     // console.log(response);
-  //     return response;
-  //   });
-  // }
-  //
-  // grabBook(index) {
-  //   return this.request('books', index);
-  // }
-
-  // grabCharacter(index) {
-  //   return this.request('characters', index);
-  // }
 
   randETag() {
     return Math.random().toString(36).split('').filter(function(value, index, self) {
